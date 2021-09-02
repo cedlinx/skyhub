08020ed034759845eb4b89530d1d9a316a9d8546
kennytech <kennyemma2008@gmail.com>
2021-09-02 16:41:52 +0100

    Bulk asset upload

--- app/Http/Controllers/AssetController.php
+++ app/Http/Controllers/AssetController.php
@@ -92,5 +92,84 @@ class AssetController extends Controller
         }
         return $randomString;
     }
+
+    public function upload_bulk_assets(Request $request)
+    {
+        $file = $request->file('file');
+
+        // File Details 
+        $filename = $file->getClientOriginalName();
+        $extension = $file->getClientOriginalExtension();
+        $tempPath = $file->getRealPath();
+        $fileSize = $file->getSize();
+        $mimeType = $file->getMimeType();
+  
+        // Valid File Extensions
+        $valid_extension = array("csv");
+  
+        // 2MB in Bytes
+        $maxFileSize = 2097152; 
+  
+        // Check file extension
+        if(in_array(strtolower($extension),$valid_extension)){
+  
+          // Check file size
+          if($fileSize <= $maxFileSize){
+  
+            // File upload location
+            $location = 'uploads';
+  
+            // Upload file
+            $file->move($location,$filename);
+  
+            // Import CSV to Database
+            $filepath = public_path($location."/".$filename);
+  
+            // Reading file
+            $file = fopen($filepath,"r");
+  
+            $importData_arr = array();
+            $i = 0;
+  
+            while (($filedata = fgetcsv($file, 1000, ",")) !== FALSE) {
+               $num = count($filedata );
+               
+               // Skip first row (Remove below comment if you want to skip the first row)
+               if($i == 0){
+                  $i++;
+                  continue; 
+               }
+               for ($c=0; $c < $num; $c++) {
+                  $importData_arr[$i][] = $filedata [$c];
+               }
+               $i++;
+            }
+            fclose($file);
+  
+            $assets = [];
+
+            // Insert to MySQL database
+            foreach($importData_arr as $importData){
+
+                $data = [
+                    'name' => $importData[0],
+                    'description' => $importData[1],
+                    'skydahid' => $this->generate_random_string(),
+                ];
+
+                array_push($assets, $data);
+
+                Asset::create($data);
+            }
+            return $this->sendSuccess('Asset successfully created', $assets);
+          }else{
+            return $this->sendError('File too large. File must be less than 2MB.', $assets = []);
+          }
+  
+        }else{
+           return $this->sendError('Invalid File Extension', $assets = []);
+        }
+  
+    }
     
 }
